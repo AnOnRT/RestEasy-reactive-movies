6f48f8ddd7fce4908992b3d5ea37d4188661a0b4
AnOnRT <arturpapyan1995@gmail.com>
2022-09-15 18:36:22 +0200

    Added UI interface by Swagger and OpenAPI

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,5 +1,13 @@
 package org.acme;
 
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -10,11 +18,22 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Path("/movies")
+@Tag(name = "Movie Resource", description = "Movie Rest API")
 public class GreetingResource {
 
     public static List<Movie> movies = new ArrayList<>();
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @Operation(
+            operationId = "getMovies",
+            summary = "Get Movies",
+            description = "Get the list of movies"
+    )
+    @APIResponse(
+        responseCode = "200",
+        description = "Operation Completed",
+        content = @Content(mediaType = MediaType.APPLICATION_JSON)
+    )
     public Response getMovie(){
         return Response.ok(movies).build();
     }
@@ -22,6 +41,16 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     @Path("/size")
+    @Operation(
+            operationId = "getMovieCount",
+            summary = "Quantity of movies",
+            description = "Get the quantity of the movies on the list"
+    )
+    @APIResponse(
+            responseCode = "200",
+            description = "Operation Completed",
+            content = @Content(mediaType = MediaType.TEXT_PLAIN)
+    )
     public Integer countMovies(){
         return movies.size();
     }
@@ -29,18 +58,52 @@ public class GreetingResource {
     @POST
     @Produces(MediaType.APPLICATION_JSON) //We reproduce our list
     @Consumes(MediaType.APPLICATION_JSON) //Consumes new movie
-    public Response createMovie(Movie newMovie){
+    @Operation(
+            operationId = "addMovie",
+            summary = "Add movie",
+            description = "Add new movie to the list"
+    )
+    @APIResponse(
+            responseCode = "201",
+            description = "Operation Completed",
+            content = @Content(mediaType = MediaType.APPLICATION_JSON)
+    )
+    public Response createMovie(
+            @RequestBody(
+                    description = "Movie to be added",
+                    required = true,
+                    content = @Content(schema = @Schema(implementation = Movie.class))
+                )
+                Movie newMovie){
         movies.add(newMovie);
-        return Response.ok(movies).build();
+        return Response.status(Response.Status.CREATED).entity(movies).build();
     }
-
 
     @PUT
     @Path("{id}/{title}")
     @Produces(MediaType.APPLICATION_JSON) //We reproduce our list
     @Consumes(MediaType.APPLICATION_JSON) //Consumes new movie
-    public Response updateMovie(@PathParam("id") Long id,
-                                @PathParam("title") String title){
+    @Operation(
+            operationId = "updateMovie",
+            summary = "Update movie",
+            description = "Update movie that is currently on the list"
+    )
+    @APIResponse(
+            responseCode = "200",
+            description = "Movie updated",
+            content = @Content(mediaType = MediaType.APPLICATION_JSON)
+    )
+    public Response updateMovie(
+            @Parameter(
+                    description =  "Movie ID",
+                    required = true
+            )
+            @PathParam("id") Long id,
+            @Parameter(
+                    description =  "Movie title ",
+                    required = true
+            )
+            @PathParam("title") String title){
 
         movies = movies.stream().map(movie -> {
             if(movie.getId().equals(id)){
@@ -56,6 +119,21 @@ public class GreetingResource {
     @DELETE
     @Path("{id}")
     @Consumes(MediaType.APPLICATION_JSON)
+    @Operation(
+            operationId = "deleteMovie",
+            summary = "Delete movie",
+            description = "Delete movie that is currently on the list"
+    )
+    @APIResponse(
+            responseCode = "204",
+            description = "Movie deleted",
+            content = @Content(mediaType = MediaType.APPLICATION_JSON)
+    )
+    @APIResponse(
+            responseCode = "404",
+            description = "Bad request, movie does not exist",
+            content = @Content(mediaType = MediaType.APPLICATION_JSON)
+    )
     public Response deleteMovie(@PathParam("id") Long id){
         
         Optional<Movie> movieToDelete = movies.stream().filter(movie -> movie.getId().equals(id))
